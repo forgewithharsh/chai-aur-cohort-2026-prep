061b641b73ad46820ff81d0a39b2b65f753432bd
Harsh Guleria <guleriah59@gmail.com>
2026-03-01 22:00:13 +0530

    feat: add makeChai promise example with resolve and reject

--- js-class/13-promises.js
+++ js-class/13-promises.js
@@ -15,5 +15,20 @@ console.log(promise);
 //   .then((data) => console.log(data))
 //   .catch((error) => console.log(error));
 
+const makeChai = new Promise((resolve, reject) => {
+  let waterBoiled = true;
 
+  if (waterBoiled) {
+    resolve("Chai is ready ☕");
+  } else {
+    reject("Water not boiled ❌");
+  }
+});
 
+makeChai
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
